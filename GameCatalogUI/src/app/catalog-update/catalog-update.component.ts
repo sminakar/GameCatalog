c719b0f358e71638203a07eba454509356375965
sminakar <722006+sminakar@users.noreply.github.com>
2021-03-21 16:33:48 -0700

    update form initiated

--- GameCatalogUI/src/app/catalog-update/catalog-update.component.ts
+++ GameCatalogUI/src/app/catalog-update/catalog-update.component.ts
@@ -4,19 +4,34 @@ import { CatalogServices } from 'src/app/shared/catalog-services.service';
 
 @Component({
   selector: 'app-catalog-update',
-  // templateUrl: './catalog-update.component.html',
-  template: '<h1>HI   From Update</h1>',
+  templateUrl: './catalog-update.component.html',
+  // template: '<h1>HI   From Update</h1>',
   styles: [
   ]
 })
 export class CatalogUpdateComponent implements OnInit {
 
+  public resultCatalogData: any;
+
   constructor(private _catalogService: CatalogServices,
     private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    let catalogID: string = this._activatedRoute.snapshot.params['id'];
-    console.log(catalogID);
+    let catalogId: string = this._activatedRoute.snapshot.params['id'];
+    
+    this._catalogService.getCatalogById(catalogId)
+    .subscribe(
+      result => 
+      {
+        if (result.isFailure)
+        {
+          console.error("Error in fatching catalog data from server");
+          return;
+        }
+        this.resultCatalogData = result.data;
+        this.resultCatalogData.releaseDate = this.resultCatalogData.releaseDate.split('T')[0];
+        
+      }, error => console.error(error)
+    )
   }
-
 }
