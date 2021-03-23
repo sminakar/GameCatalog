e5004ba5162a0fc2ecadc83d2d1ceaa978ed62cc
sminakar <722006+sminakar@users.noreply.github.com>
2021-03-23 12:30:19 -0700

    Unit tests completes

--- GameCatalog.Test/UnitTest1.cs
+++ GameCatalog.Test/UnitTest1.cs
@@ -223,21 +223,18 @@ namespace GameCatalog.Test
                 ReleaseDate = DateTime.UtcNow
             };
 
-            var carToAddJson = JsonConvert.SerializeObject(catToAdd);
-            var content = new StringContent(carToAddJson,
+            var catToAddJson = JsonConvert.SerializeObject(catToAdd);
+            var content = new StringContent(catToAddJson,
                 Encoding.UTF8,
                 "application/json");
 
-            var request = new HttpRequestMessage(HttpMethod.Get,
-                $"{BaseURL}AddCatalog");
-
             var response = await _client.PostAsync($"{BaseURL}AddCatalog", content);
-            string result = response.Content.ReadAsStringAsync().Result;
 
+            System.Net.HttpStatusCode actual = response.StatusCode;
+            Assert.Equal(System.Net.HttpStatusCode.OK, actual);
+
+            string result = response.Content.ReadAsStringAsync().Result;
             string addedCatalogId = (JObject.Parse(result))["CId"].ToString();
-            
-            int actual = (int)response.StatusCode;
-            Assert.Equal(200, actual);
 
             // Retrieve directly stored Dummy from db 
             CatalogVM dbCatalog = await _repository.GetCatalogByIdAsync(Guid.Parse(addedCatalogId));
@@ -248,6 +245,184 @@ namespace GameCatalog.Test
             _repository.DeleteCatalogByIdAsync(Guid.Parse(addedCatalogId));
         }
 
+
+        [Fact]
+        public async void AddCatalogByAPI_WithWronData_ShoulReturnBadRequestAsync()
+        {
+            // Store Dummy to database by API with Wrong Company Id
+
+            var catCompany = (await _repository.GetDevelopersAsync())
+                .First(c => c.ClusterID == 1);
+
+            var catGenre = (await _repository.GetGenresAsync())
+                .First(g => g.ID == 1);
+
+            Guid id = Guid.NewGuid();
+
+            CatalogVM catToAdd = new CatalogVM()
+            {
+                ID = id,
+                Title = $"DummyTitle {id}",
+                CompanyID = Guid.Empty,          // wrong Id
+                CompanyName = catCompany.CompanyName,
+                GenreID = catGenre.ID,
+                GenereTitle = catGenre.Title,
+                Price = 10.0M,
+                Rate = 5,
+                ReleaseDate = DateTime.UtcNow
+            };
+
+            var catToAddJson = JsonConvert.SerializeObject(catToAdd);
+            var content = new StringContent(catToAddJson,
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _client.PostAsync($"{BaseURL}AddCatalog", content);
+
+            System.Net.HttpStatusCode actual = response.StatusCode;
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, actual);
+
+            catToAdd.CompanyID = catCompany.ID;
+            catToAdd.CompanyName = catCompany.CompanyName;
+
+            catToAdd.GenreID = 0; // this time set request with wron GenreId
+
+            response = await _client.PostAsync($"{BaseURL}AddCatalog", content);
+
+            actual = response.StatusCode;
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, actual);
+
+            catToAdd.GenreID = catGenre.ID;
+
+            catToAdd.Title = String.Empty;  // this time send the request with wrong game title
+
+            response = await _client.PostAsync($"{BaseURL}AddCatalog", content);
+
+            actual = response.StatusCode;
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, actual);
+        }
+
+
+        [Fact]
+        public async void TryToUpdateACatalogWithWrongCompanyIDByAPI_ShouldReturnBadRequestAsync()
+        {
+            var catalogToUpdate = _repository.GetCatalogsAsync()
+                .Result
+                .FirstOrDefault(c => c.ClusterID == 1);     // retrieve a catalog
+
+            catalogToUpdate.ID = Guid.Empty;        // assign a wrong ID
+
+            var catToUpdateJson = JsonConvert.SerializeObject(catalogToUpdate);
+            var content = new StringContent(catToUpdateJson,
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _client.PutAsync($"{BaseURL}UpdateCatalog", content);
+
+            System.Net.HttpStatusCode actual = response.StatusCode;
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, actual);
+        }
+
+
+        [Theory]
+        [InlineData("title", "Title to Update")]
+        [InlineData("rate", 3)]
+        [InlineData("price", 12.66)]
+        [InlineData("genreID", 1)]
+        [InlineData("companyID", "EED48690-A564-4208-9FC6-16610270BE8E")]
+        
+        public async void UpdateACatalogsByAPI_ShouldReturnOkAsync(string fieldName, object newFieldValue)
+        {
+          
+            PropertyInfo[] pinfo = (typeof(CatalogVM)).GetProperties();
+
+            // select a random catalog record from database
+            var dbCatalogs = _repository.GetCatalogsAsync().Result.ToList();
+            int selectedIndex = new Random().Next(0, dbCatalogs.Count - 1);
+            var catToUpdate = dbCatalogs[selectedIndex];
+            object storedFieldValue = null;
+
+            foreach(var property in pinfo)
+            {
+                if (property.Name.ToUpper() == fieldName.ToUpper())
+                {
+                    PropertyInfo prop = catToUpdate.GetType().GetProperty((property.Name));
+                    storedFieldValue = prop.GetValue(catToUpdate);    // keep the current field value
+                    
+                    switch (prop.PropertyType.Name)
+                    {
+                        case "Decimal":
+                            prop.SetValue(catToUpdate, decimal.Parse(newFieldValue.ToString()));
+                            break;
+
+                        case "Guid":
+                            prop.SetValue(catToUpdate, Guid.Parse(newFieldValue.ToString()));
+                            break;
+
+                        default:
+                            prop.SetValue(catToUpdate, newFieldValue);
+                            break;
+                    }
+                }
+            }
+
+            var catToUpdateJson = JsonConvert.SerializeObject(catToUpdate);
+            var content = new StringContent(catToUpdateJson,
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _client.PutAsync($"{BaseURL}UpdateCatalog", content);
+
+            System.Net.HttpStatusCode actual = response.StatusCode;
+            Assert.Equal(System.Net.HttpStatusCode.OK, actual);
+
+            // check if database recored is changed
+            var dbfCatalog = await _repository.GetCatalogByIdAsync(catToUpdate.ID);
+            var expected = newFieldValue;
+            object actualInDb = null;
+
+            foreach (var property in pinfo)
+            {
+                if (property.Name.ToUpper() == fieldName.ToUpper())
+                {
+                    actualInDb = property.GetValue(dbfCatalog);
+                    Assert.Equal(expected.ToString().ToUpper(), actualInDb.ToString().ToUpper());
+
+                    break;
+                }
+            }
+
+            // Restore field value to its original
+            foreach (var property in pinfo)
+            {
+                if (property.Name.ToUpper() == fieldName.ToUpper())
+                {
+                    switch (property.PropertyType.Name)
+                    {
+                        case "Decimal":
+                            property.SetValue(catToUpdate, decimal.Parse(storedFieldValue.ToString()));
+                            break;
+
+                        case "Guid":
+                            property.SetValue(catToUpdate, Guid.Parse(storedFieldValue.ToString()));
+                            break;
+
+                        default:
+                            property.SetValue(catToUpdate, newFieldValue);
+                            break;
+                    }
+
+                    break;
+                }
+            }
+
+            catToUpdateJson = JsonConvert.SerializeObject(catToUpdate);
+            content = new StringContent(catToUpdateJson,
+                Encoding.UTF8,
+                "application/json");
+
+            await _client.PutAsync($"{BaseURL}UpdateCatalog", content);
+        }
     }
 }
 
